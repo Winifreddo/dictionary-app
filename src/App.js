22e643d2c968834fd0a665c556abc4e726d1b358
Winifreddo <73237857+Winifreddo@users.noreply.github.com>
2021-03-20 17:35:43 +0000

    added logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import logo from "./logo.png";
 import './App.css';
 
 
 export default function App() {
   return (
-    <div className="Dictionary">
-      <h1>Hello</h1>
+    <div className="logo">
+      <header>
+       <img src={logo} className="app-header" alt="logo" /> 
+      </header>
     </div>
    
   );
