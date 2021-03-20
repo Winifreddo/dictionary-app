c72c433028f81072b18df9ae6d15c1574e9b8c85
Winifreddo <73237857+Winifreddo@users.noreply.github.com>
2021-03-20 17:44:01 +0000

    Added open source link

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import './App.css';
 
 export default function App() {
   return (
+    <div className="container">
     <div className="logo">
       <header>
        <img src={logo} className="app-header" alt="logo" /> 
       </header>
+      <footer>
+        Coded by Leigh Richards, open-sourced on<a href="https://github.com/Winifreddo/dictionary-app">gitHub</a>  
+      </footer>
     </div>
-   
+   </div>
   );
 }
 
