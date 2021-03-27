a4c85e201e8e81d0f45ab719f7dc165f94bf320e
Winifreddo <73237857+Winifreddo@users.noreply.github.com>
2021-03-27 18:50:10 +0000

    updated link

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -7,7 +7,7 @@ export default function Phonetic (props) {
     return (
         
         <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank">
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
             <img src={listen} className="Listen" alt="Listen" /> 
             </a>
             
